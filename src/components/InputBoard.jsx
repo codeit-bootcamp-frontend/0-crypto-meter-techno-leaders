83e552524b9a6e81e72c880d5a8638d2a9f762b4
sally <9609sally@gmail.com>
2023-05-15 23:13:05 +0900

    feat: submit 버튼 누르면 콘솔창 출력

--- src/components/InputBoard.jsx
+++ src/components/InputBoard.jsx
@@ -65,46 +65,58 @@ function InputBoard() {
     handleChange('amount', newAmount);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(values);
+  };
+
   return (
     <div className="inputBoardContainer">
       <InputBoardTitle values={values} />
-      <ReactDatePicker
-        name="selectedDate"
-        locale={'ko'}
-        selected={values.selectedDate}
-        onChange={handleDateChange}
-        showMonthDropdown
-        showYearDropdown
-        dropdownMode="select"
-        dateFormat="yyyy년 MM월 dd일"
-        dateFormatCalendar="yyyy MMMM"
-      />
-      <input
-        name="amount"
-        type="number"
-        value={values.amount}
-        onChange={handleAmountChange}
-      />
-      <div className="addButtons">
-        <button value={5000} onClick={addTotalAmount}>
-          5000원
-        </button>
-        <button value={10000} onClick={addTotalAmount}>
-          10000원
-        </button>
-        <button value={50000} onClick={addTotalAmount}>
-          50000원
-        </button>
-        <button value={100000} onClick={addTotalAmount}>
-          100000원
-        </button>
-      </div>
-      <select name="crypto" value={values.crypto} onChange={handleCryptoChange}>
-        <option value="Bitcoin">BitCoin</option>
-        <option value="Ethereum">Ethereum</option>
-        <option value="Tether">Tether</option>
-        <option value="Dogecoin">Dogecoin</option>
-      </select>
+      <form className="boardInput" onSubmit={handleSubmit}>
+        <ReactDatePicker
+          name="selectedDate"
+          locale={'ko'}
+          selected={values.selectedDate}
+          onChange={handleDateChange}
+          showMonthDropdown
+          showYearDropdown
+          dropdownMode="select"
+          dateFormat="yyyy년 MM월 dd일"
+          dateFormatCalendar="yyyy MMMM"
+        />
+        <input
+          name="amount"
+          type="number"
+          value={values.amount}
+          onChange={handleAmountChange}
+        />
+        <div className="addButtons">
+          <button value={5000} onClick={addTotalAmount}>
+            5000원
+          </button>
+          <button value={10000} onClick={addTotalAmount}>
+            10000원
+          </button>
+          <button value={50000} onClick={addTotalAmount}>
+            50000원
+          </button>
+          <button value={100000} onClick={addTotalAmount}>
+            100000원
+          </button>
+        </div>
+        <select
+          name="crypto"
+          value={values.crypto}
+          onChange={handleCryptoChange}
+        >
+          <option value="Bitcoin">BitCoin</option>
+          <option value="Ethereum">Ethereum</option>
+          <option value="Tether">Tether</option>
+          <option value="Dogecoin">Dogecoin</option>
+        </select>
+        <button type="submit">오늘 얼마가 되었을까?</button>
+      </form>
     </div>
   );
 }
