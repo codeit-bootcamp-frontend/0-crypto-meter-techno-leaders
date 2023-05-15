9ca34016ca5036de6581f9b9149837754728c824
sally <9609sally@gmail.com>
2023-05-15 20:27:45 +0900

    feat: inputBoardTitle 기본 틀 구성

--- src/components/InputBoard.jsx
+++ src/components/InputBoard.jsx
@@ -1,3 +1,20 @@
 function InputBoard() {
-  return <></>;
+  return (
+    <div className="inputBoardContainer">
+      <div className="inputBoardTitle">
+        <p>
+          내가 만약
+          <br />
+          <span>2022년 10월 12일</span>에
+        </p>
+        <p>
+          <span>15,000원</span>으로
+          <br />
+          <span>Bitcoin</span>을 샀다면,
+        </p>
+      </div>
+    </div>
+  );
 }
+
+export default InputBoard;
