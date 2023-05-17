c5b0837e37b0f844754ef269e77e36bfb1a8d481
sally <9609sally@gmail.com>
2023-05-17 22:30:06 +0900

    feat: mockjson 파일 배열 렌더링

--- src/components/InputBoard.jsx
+++ src/components/InputBoard.jsx
@@ -3,6 +3,8 @@ import ReactDatePicker, { registerLocale } from 'react-datepicker';
 import ko from 'date-fns/locale/ko';
 import InputBoardTitle from '/src/components/InputBoardTitle';
 
+import cryptoCoins from '/src/assets/coins_markets.json';
+
 // react-datepicker 캘린더 드롭다운 기본 css 모듈
 import 'react-datepicker/dist/react-datepicker.css';
 
@@ -87,10 +89,14 @@ function InputBoard() {
           value={values.crypto}
           onChange={handleInputChange}
         >
-          <option value="Bitcoin">BitCoin</option>
-          <option value="Ethereum">Ethereum</option>
-          <option value="Tether">Tether</option>
-          <option value="Dogecoin">Dogecoin</option>
+          {cryptoCoins.map((item) => {
+            const { id, name, image } = item;
+            return (
+              <option key={id} value={name}>
+                {name}
+              </option>
+            );
+          })}
         </select>
         <button type="submit">오늘 얼마가 되었을까?</button>
       </form>
