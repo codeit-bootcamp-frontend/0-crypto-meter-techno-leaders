ae3d0181ad1384f64ad5062c50bcd3794c3d1a66
hayoung-99 <nhy990130@gmail.com>
2023-05-17 17:55:48 +0900

    add: default 데이터 설정

--- src/components/MainBoard.jsx
+++ src/components/MainBoard.jsx
@@ -1,7 +1,13 @@
 import CoinChart from '/src/components/CoinChart';
 import coinPrices from '/src/assets/coins_bitcoin_marketcharts.json';
 
-function MainBoard() {
+const PREV_DATE = new Date('2022-05-12');
+
+const DEFAULT_VALUES = {
+  selectedDate: PREV_DATE,
+  amount: 15000,
+  crypto: 'Bitcoin',
+};
   return (
     <div style={{ aspectRatio: 2 }}>
       <CoinChart data={coinPrices.prices} />
