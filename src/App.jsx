9eec3525c9cb33ac163143ad3aeb426366e904f5
Andy-Shin99 <tlsalsrb990713@gmail.com>
2023-05-19 17:46:07 +0900

    feat: CurrencyProvider 컴포넌트로 context 범위 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import './App.css';
 import GNB from './components/GNB/GNB';
+import { CurrencyProvider } from './contexts/CurrencyContext';
 
 function App() {
-  return <GNB />;
+  return (
+    <CurrencyProvider value={'krw'}>
+      <GNB />
+    </CurrencyProvider>
+  );
 }
 
 export default App;
