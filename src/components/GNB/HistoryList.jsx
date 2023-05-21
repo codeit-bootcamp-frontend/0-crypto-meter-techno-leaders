bcdd4939db20402314b7876aca2a5c75783bbdc2
Andy-Shin99 <tlsalsrb990713@gmail.com>
2023-05-21 22:23:51 +0900

    feat: HistoryListItem 컴포넌트 key prop 설정

--- src/components/GNB/HistoryList.jsx
+++ src/components/GNB/HistoryList.jsx
@@ -8,20 +8,35 @@ const dateString = (years, months, days) => {
   return `${year}년 ${month}월 ${day}일`;
 };
 
-const data = {
-  purchaseDate: dateString(2020, 1, 28),
-  investment: 10000,
-  coinName: 'bitcoin',
-  coinLogoURL:
-    'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579',
-  currentDate: dateString(2023, 5, 18),
-  currentValue: 30000,
-};
+const data = [
+  {
+    id: 'bitcoin',
+    purchaseDate: dateString(2020, 1, 28),
+    investment: 10000,
+    coinName: 'bitcoin',
+    coinLogoURL:
+      'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579',
+    currentDate: dateString(2023, 5, 18),
+    currentValue: 30000,
+  },
+  {
+    id: 'ethurium',
+    purchaseDate: dateString(2020, 1, 28),
+    investment: 20000,
+    coinName: 'ethurium',
+    coinLogoURL:
+      'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579',
+    currentDate: dateString(2023, 5, 18),
+    currentValue: 30000,
+  },
+];
 
 function HistoryList() {
   return (
     <ul style={{ margin: 0, paddingLeft: '24px' }}>
-      <HistoryListItem datas={data} />
+      {data.map((item) => (
+        <HistoryListItem key={item.id} datas={item} />
+      ))}
     </ul>
   );
 }
