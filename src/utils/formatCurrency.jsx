0877cd1691b278c3790f54cafc9e9572c6f22ee8
sally <9609sally@gmail.com>
2023-05-23 05:07:20 +0900

    style: formatCurrency 템플릿문자열로 변경

--- src/utils/formatCurrency.jsx
+++ src/utils/formatCurrency.jsx
@@ -1,9 +1,9 @@
 export function formatCurrency(money, currencyContext) {
   switch (currencyContext) {
     case 'krw':
-      return money.toLocaleString() + '원';
+      return `${money.toLocaleString()}원`;
     case 'usd':
-      return '$' + money.toLocaleString();
+      return `$${money.toLocaleString()}`;
     default:
       return '지원하지 않는 통화입니다';
   }
