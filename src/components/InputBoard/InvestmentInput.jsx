91bef19f2cd7c0a45d8989334b590eb46b8b2e2b
sally <9609sally@gmail.com>
2023-05-23 04:49:30 +0900

    feat: investment input 통화 표시 구현

--- src/components/InputBoard/InvestmentInput.jsx
+++ src/components/InputBoard/InvestmentInput.jsx
@@ -16,17 +16,22 @@ function InvestmentInput({ investment, onChange }) {
   };
 
   return (
-    <>
+    <div className={styles.inputWrapper}>
       <NumericFormat
         className={styles.input}
         value={investment}
         thousandSeparator
         onValueChange={handleInvestmentChange}
       />
+      {currency === 'krw' ? (
+        <div className={styles.currency}>원 (₩)</div>
+      ) : (
+        <div className={styles.currency}>USD ($)</div>
+      )}
       <div className={styles.buttonsContainer} onClick={addTotalInvestment}>
         {currency === 'krw' ? <KrwButtons /> : <UsdButtons />}
       </div>
-    </>
+    </div>
   );
 }
 
