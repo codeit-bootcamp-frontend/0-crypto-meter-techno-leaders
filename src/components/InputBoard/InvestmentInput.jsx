19f9b4cb4e7955dc27186cb45362a9c61f0e01a7
sally <9609sally@gmail.com>
2023-05-23 04:14:34 +0900

    feat: investment input 쉼표 구현

--- src/components/InputBoard/InvestmentInput.jsx
+++ src/components/InputBoard/InvestmentInput.jsx
@@ -1,12 +1,13 @@
 import { useCurrency } from '/src/contexts/CurrencyContext';
 import { KrwButtons, UsdButtons } from '/src/components/InputBoard/AddButtons';
 import styles from '/src/components/InputBoard/InvestmentInput.module.css';
+import { NumericFormat } from 'react-number-format';
 
 function InvestmentInput({ investment, onChange }) {
   const currency = useCurrency();
 
-  const handleInvestmentChange = (e) => {
-    onChange(+e.target.value);
+  const handleInvestmentChange = ({ value }) => {
+    onChange(+value);
   };
 
   const addTotalInvestment = (e) => {
@@ -16,11 +17,10 @@ function InvestmentInput({ investment, onChange }) {
 
   return (
     <>
-      <input
-        name="investment"
-        type="number"
+      <NumericFormat
         value={investment}
-        onChange={handleInvestmentChange}
+        thousandSeparator
+        onValueChange={handleInvestmentChange}
       />
       <div className={styles.buttonsContainer} onClick={addTotalInvestment}>
         {currency === 'krw' ? <KrwButtons /> : <UsdButtons />}
