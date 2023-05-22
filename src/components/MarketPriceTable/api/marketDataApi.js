f29fc021cd09f9544ae238843f40294bbedf33dd
redcoin96 <redcoin96@naver.com>
2023-05-22 17:59:33 +0900

    style: api axios로 변경하여 fetch

--- src/components/MarketPriceTable/api/marketDataApi.js
+++ src/components/MarketPriceTable/api/marketDataApi.js
@@ -1,11 +1,19 @@
-const apiKey = import.meta.env.VITE_COINGECKO_KEY;
+import axiosInstance from '/src/api/axiosInstance.js';
 
 const fetchMarketData = async (page, currency) => {
   try {
-    const response = await fetch(
-      `https://pro-api.coingecko.com/api/v3/coins/markets?x_cg_pro_api_key=${apiKey}&vs_currency=${currency}&order=market_cap_desc&per_page=240&page=${page}&sparkline=false&price_change_percentage=1h%2C24h%2C7d&locale=en`
-    );
-    const data = await response.json();
+    const response = await axiosInstance.get(`/coins/markets`, {
+      params: {
+        vs_currency: currency,
+        order: 'market_cap_desc',
+        per_page: 240,
+        page: page,
+        sparkline: false,
+        price_change_percentage: '1h,24h,7d',
+        locale: 'en',
+      },
+    });
+    const data = response.data;
     return data;
   } catch (error) {
     console.log(error);
