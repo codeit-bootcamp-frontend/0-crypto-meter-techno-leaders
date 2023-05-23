e7c53c0fe37f5b525b5db625934c6ce185387417
Andy-Shin99 <tlsalsrb990713@gmail.com>
2023-05-23 16:34:38 +0900

    refactor: 불필요한 날짜 formatting 함수 삭제

--- src/components/GNB/HistoryList.jsx
+++ src/components/GNB/HistoryList.jsx
@@ -1,13 +1,5 @@
 import HistoryListItem from '/src/components/GNB/HistoryListItem';
 
-const dateString = (years, months, days) => {
-  const newDate = new Date(years, months, days);
-  const year = newDate.getFullYear();
-  const month = newDate.getMonth() - 1;
-  const day = newDate.getDate();
-  return `${year}년 ${month}월 ${day}일`;
-};
-
 const data = [];
 
 function HistoryList() {
