22572dcc609cfcf219bcc2c981ded199c7945e52
Andy-Shin99 <tlsalsrb990713@gmail.com>
2023-05-23 11:53:10 +0900

    chore: HistoryListItem 생성시 활용될 data 배열의 mock-data 삭제

--- src/components/GNB/HistoryList.jsx
+++ src/components/GNB/HistoryList.jsx
@@ -8,28 +8,7 @@ const dateString = (years, months, days) => {
   return `${year}년 ${month}월 ${day}일`;
 };
 
-const data = [
-  {
-    id: 'bitcoin',
-    purchaseDate: dateString(2020, 1, 28),
-    investment: 10000,
-    coinName: 'bitcoin',
-    coinLogoURL:
-      'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579',
-    currentDate: dateString(2023, 5, 18),
-    currentValue: 30000,
-  },
-  {
-    id: 'ethurium',
-    purchaseDate: dateString(2020, 1, 28),
-    investment: 20000,
-    coinName: 'ethurium',
-    coinLogoURL:
-      'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579',
-    currentDate: dateString(2023, 5, 18),
-    currentValue: 30000,
-  },
-];
+const data = [];
 
 function HistoryList() {
   return (
