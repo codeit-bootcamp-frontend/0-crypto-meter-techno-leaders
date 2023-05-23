5e0cc2209a5eca5ee2515ce1401c9c0b3b92e7d1
Andy-Shin99 <tlsalsrb990713@gmail.com>
2023-05-23 11:48:22 +0900

    feat: Logo 컴포넌트 스타일 분리 및 반응형 구현

--- src/components/GNB/Logo.jsx
+++ src/components/GNB/Logo.jsx
@@ -1,19 +1,28 @@
+import { useMediaQuery } from 'react-responsive';
 import imgLogo from '/src/assets/images/imgLogo.svg';
 import textLogo from '/src/assets/images/textLogo.svg';
+import { Mobile, TabletAbove } from './MediaQuery';
+import classNames from 'classnames/bind';
+import styles from '/src/components/GNB/Logo.module.css';
+
+const cn = classNames.bind(styles);
 
 function Logo() {
+  const isMobile = useMediaQuery({
+    query: '(max-width:767px)',
+  });
+
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        gap: '11px',
-      }}
-    >
-      <img src={imgLogo} />
-      <img src={textLogo} />
-    </div>
+    <a href="/">
+      <div className={cn('logo-container')}>
+        <Mobile>
+          <img src={imgLogo} />
+        </Mobile>
+        <TabletAbove>
+          <img src={imgLogo} /> <img src={textLogo} />
+        </TabletAbove>
+      </div>
+    </a>
   );
 }
 
