25b2a5822a20f626dedf01007e2866837628849a
sally <9609sally@gmail.com>
2023-05-23 19:14:30 +0900

    refactor: App 공백 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import GNB from '/src/components/GNB/GNB';
 import InputBoard from '/src/components/InputBoard/InputBoard';
 import MainBoard from '/src/components/MainBoard/MainBoard';
 import MarketPriceTable from '/src/components/MarketPriceTable/MarketPriceTable_api.jsx';
-
 import '/src/App.css';
 
 registerLocale('ko', ko);
