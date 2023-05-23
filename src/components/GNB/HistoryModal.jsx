f38814b5564797a2d1a66652bb3346e8642c2105
Andy-Shin99 <tlsalsrb990713@gmail.com>
2023-05-23 11:49:35 +0900

    feat: HistoryModal 컴포넌트 스타일 분리 및 반응형 구현

--- src/components/GNB/HistoryModal.jsx
+++ src/components/GNB/HistoryModal.jsx
@@ -1,58 +1,48 @@
 import { useCallback } from 'react';
+import { useMediaQuery } from 'react-responsive';
 import Modal from 'react-modal';
 import Button from '/src/components/GNB/Button';
 import HistoryList from '/src/components/GNB/HistoryList';
+import {
+  DesktopModalStyle,
+  TabletModalStyle,
+  MobileModalStyle,
+} from '/src/components/GNB/ModalStyles';
+import classNames from 'classnames/bind';
+import styles from '/src/components/GNB/HistoryModal.module.css';
 
-const modalStyle = {
-  overlay: {
-    top: '0',
-    right: '0',
-    backgroundColor: 'none',
-  },
-  content: {
-    position: 'absolute',
-    overflow: 'auto',
-    top: '76px',
-    right: '52px',
-    left: 'auto',
-    bottom: 'auto',
-    border: '1px solid #E7E9F0',
-    borderRadius: '16px',
-    boxShadow: '0px 4px 15px rgba(11, 14, 27, 0.08)',
-    zIndex: 10,
-    width: '520px',
-    height: '590px',
-    padding: 0,
-  },
-};
+const cn = classNames.bind(styles);
 
 function HistoryModal({ isOpen, handleModalOpen }) {
   const handleModalClose = useCallback(() => {
     handleModalOpen(false);
   }, []);
 
+  const isMobile = useMediaQuery({
+    query: '(max-width: 620px)',
+  });
+
+  const isTablet = useMediaQuery({
+    query: '(max-width: 767px) and (min-width: 621px)',
+  });
+
   return (
     <Modal
-      style={modalStyle}
+      style={
+        isMobile
+          ? MobileModalStyle
+          : isTablet
+          ? TabletModalStyle
+          : DesktopModalStyle
+      }
       isOpen={isOpen}
       onRequestClose={() => handleModalClose()}
     >
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          width: '464px',
-          borderBottom: '1px solid #E7E9F0',
-          paddingLeft: '28px',
-        }}
-      >
-        <h1 style={{ fontSize: '20px', fontWeight: 700, width: '73px' }}>
-          검색
-        </h1>
+      <div className={cn('modal-header')}>
+        <h1 className={cn('modal-title')}>검색기록</h1>
         <Button
-          propStyle={{ border: 'none', width: '90px' }}
-          name="기록 모두 "
+          propStyle={{ border: 'none', width: '95px' }}
+          name="기록 모두 지우기"
         />
       </div>
       <HistoryList></HistoryList>
