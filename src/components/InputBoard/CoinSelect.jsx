90ed4a8c4d6a3924bc2fd7a643d14a09a7ca47e0
09sally <9609sally@gmail.com>
2023-05-24 17:15:10 +0900

    feat: CoinSelect  스타일 적용 몇 개

--- src/components/InputBoard/CoinSelect.jsx
+++ src/components/InputBoard/CoinSelect.jsx
@@ -1,10 +1,20 @@
 import Select, { components } from 'react-select';
+import styles from '/src/components/InputBoard/CoinSelect.module.css';
 import coinsList from '/src/assets/coins_markets.json';
 
 const options = coinsList.map((coin) => {
   const { id, name, image } = coin;
   return { value: id, label: name, image: image };
 });
+
+function CustomOption({ innerProps, label, data }) {
+  return (
+    <div {...innerProps} className={styles.coinOption}>
+      <img className={styles.coinImg} src={data.image} />
+      {label}
+    </div>
+  );
+}
 
 function CustomControl({ children, ...props }) {
   const selectedOption = props.selectProps.value;
@@ -15,15 +25,6 @@ function CustomControl({ children, ...props }) {
       {image && <img src={image} style={{ width: '24px', height: 'auto' }} />}
       {children}
     </components.Control>
-  );
-}
-
-function CustomOption({ innerProps, label, data }) {
-  return (
-    <div {...innerProps}>
-      <img src={data.image} style={{ width: '24px', height: 'auto' }} />
-      {label}
-    </div>
   );
 }
 
